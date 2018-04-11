7e86b6f6d75de7357f04345ea5189e01fa6b7130
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-04-11 06:56:09 -0400

    uploading yesterday's classwork

---
+++ classes/Apr10/boring.js
@@ -0,0 +1,59 @@
+// alert("It's alive!!!!!!")
+
+
+// let prices = [33,26,99,120,12,45];
+// let sum = 0;
+
+// for (i=0; i < prices.length; i++){	 
+//    sum += prices[i]
+// };
+
+// console.log(sum);
+
+
+// let crows = 99;
+
+
+
+
+
+//     while( crows >1){
+// 	        console.log( `${crows} on the wall. ${crows}. 1 fell down, and became wight.`);
+// 	               crows--;
+
+//                    } ;
+//         if (crows == 1){
+//         	console.log("we did it!")
+//         }
+
+
+// let plus = "+";
+// let sum = 0;
+// while( 10 > sum){
+//       console.log(plus);
+//       sum++;
+//      };
+
+
+
+ let nums = [2, 7, 11, 15];
+
+ for( i=0; i< nums.length; i++){
+ 	for(j=1+1; j<nums.length[i]; J++){
+           if( nums[i] + nums[j] == 9){
+           	  console.log(nums[i], nums[j]);
+           };
+ 	}
+ };
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ classes/Apr10/practice.js
@@ -0,0 +1,92 @@
+// alert(
+// 	"M'bop");
+
+// console.log(
+// 	"Dowop");
+
+// const name = "Alfonso";
+// let myAge = "35";
+
+// console.log(
+// 	`My name is ${name} and I am ${myAge} years old.`);
+
+
+// let mathOne = 20;
+// let mathTwo = 3;
+// let mathSum = mathOne + mathTwo;
+// let mathProduct = mathOne * mathTwo;
+// console.log(
+// 	mathSum, mathProduct);
+
+
+
+// let students = ["a","b","c","d","e"];
+// console.log (students[2]);
+
+
+// let multi = [
+//   ["hello","goodbye","food" ],
+//   ["something","else","here"],
+//   ["beans","fruits","veggies"]
+// ]
+// console.log( multi[2][0]);
+
+
+
+// let bob = {
+//     eyeColor :"brown",
+//     height: "6 feet"
+// };
+
+
+// let movies = { topMovieOne: "I'm gonna git you Sucka", topMovieTwo: "The Wiz", topMovieThree: "The Last Dragon;"};
+// console.log(`My favorite movies are ${movies.topMovieOne} and ${movies.topMovieTwo}, but I really enjoy ${movies.topMovieThree}`);
+
+
+
+// let i = 9;
+
+// if(i < 10){
+//   alert("This is less than 10, fool.");
+// } else if (i >10 ){
+// 	alert("This is greater than 10, sucka.");
+// } else {
+// 	alert("This is 10, but you're still a sucka, fool.");
+// }
+
+
+
+
+// let temp = 46;
+// let acStatus = "on";
+
+// if( temp > 80 && acStatus ==="off"){
+//    console.log("Turn on the AC!")
+// } else if (temp > 80 && acStatus ==="on"){
+// 	console.log("This thing is broken")
+// } else if (temp < 60 && acStatus==="on"){
+// 	console.log("brrr, turn this thing off!")
+// } else if (temp <80 && acStatus==="off"){
+// 	console.log("not worth the elecricity; leave it off.")
+// } else {
+// 	console.log("wha???")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
