fdca9433beeacfbd27bb7198107f3cd6ccd70b81
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-04-19 14:08:44 -0400

    new day, new class

---
+++ classes/Apr18/main.js
@@ -0,0 +1,13 @@
+// alert("hey hey hey!");
+
+
+let turnRed = document.querySelector('#submit-button');
+let inputBorder = document.querySelector('#input-important');
+
+turnRed.addEventListener('click', function(){
+        inputBorder.style.borderColor="red";
+   });
+
+
+let gotBars = document.querySelectorAll(".nycda-class");
+
---
+++ classes/Apr19/main.js
@@ -0,0 +1,9 @@
+let onOff = document.getElementById('myBtn');
+
+
+
+onOff.addEventListener('onclick', function(){
+     alert("I'm ALLLLLLLIIIIIVEEEEE!!!!");
+ });
+
+
